93d5d8fec417a18dfee8eed7835273f04d88409f
Laurence Roberts <lsjroberts@gmail.com>
2014-06-16 13:29:07 +0100

    Fixed gravatar

--- src/Controller/Module/Avatar.php
+++ src/Controller/Module/Avatar.php
@@ -7,9 +7,21 @@ use Message\Cog\Controller\Controller;
 
 class Avatar extends Controller
 {
-	public function index(UserInterface $user, $size = 100, $default = 'identicon')
+	/**
+	 * Create a hash and get the default image for a gravatar.
+	 *
+	 * Gravatar does not render local images correctly, instead for local
+	 * environments we will just render an identicon.
+	 */
+	public function index(UserInterface $user, $size = 100, $default = null)
 	{
 		$hash = md5(strtolower(trim($user->email)));
+
+		if ('local' == $this->get('env')) {
+			$default = 'identicon';
+		} else {
+			$default = ($default) ?: urlencode('/cogules/Message:Mothership:User/images/avatar.png');
+		}
 
 		return $this->render('Message:Mothership:User::module:avatar', [
 			'hash'    => $hash,
