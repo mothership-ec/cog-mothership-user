24f85fc9f68f169ecb019be15f5e0643a332e905
thomasjthomasj <thomasjmarchant@hotmail.com>
2015-01-16 14:45:37 +0000

    moved the majority of address code from commerce to ms user, and made the tabs a bit smarter in the admin panel

--- src/Bootstrap/Services.php
+++ src/Bootstrap/Services.php
@@ -17,6 +17,41 @@ class Services implements ServicesInterface
 				new User\Avatar\Gravatar,
 			]);
 		};
+
+		$services['user.address.types'] = function($c) {
+			return [];
+		};
+
+		$services['user.address.create'] = function($c) {
+			return new User\Address\Create($c['db.query'], $c['user.address.loader'], $c['user.current']);
+		};
+
+		$services['user.address.loader'] = function($c) {
+			return new User\Address\Loader(
+				$c['db.query'],
+				$c['country.list'],
+				$c['state.list']
+			);
+		};
+
+		$services['user.address.edit'] = function($c) {
+			return new User\Address\Edit($c['db.query'], $c['user.current']);
+		};
+
+		$services['user.address.delete'] = function($c) {
+			return new User\Address\Delete($c['db.query']);
+		};
+
+		$services['user.tabs'] = function($c) {
+			$tabs = [];
+			$tabs['ms.cp.user.admin.detail.edit'] = 'Details';
+			if (count($c['user.address.types'])) {
+				$tabs['ms.cp.user.admin.address.edit'] = 'Addresses';
+			}
+			$tabs['ms.cp.user.admin.groups.edit'] = 'Groups';
+
+			return $tabs;
+		};
 	}
 
 	public function registerReports($services)
