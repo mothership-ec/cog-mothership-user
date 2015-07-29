0aaaebbb00292dd9a393ce72346eaba6ab7d9b3b
thomasjthomasj <thomasjmarchant@hotmail.com>
2015-07-29 17:42:28 +0100

    dont bother using constant for profile route

--- src/Controller/User/Tabs.php
+++ src/Controller/User/Tabs.php
@@ -6,15 +6,13 @@ use Message\Cog\Controller\Controller;
 
 class Tabs extends Controller
 {
-	const PROFILE_ROUTE = 'ms.cp.user.admin.profile';
-
 	public function tabs($userID)
 	{
 		$tabs = $this->get('user.tabs');
 		$userType = $this->get('user.profile.type.loader')->getByUserID($userID);
 
 		if ($userType->getName() === 'none') {
-			unset($tabs[self::PROFILE_ROUTE]);
+			unset($tabs['ms.cp.user.admin.profile']);
 		}
 
 		return $this->render('Message:Mothership:User::user:listing:tabs', [
